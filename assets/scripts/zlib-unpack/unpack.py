6ae516fdf313f13e0fc41ce10b7b750e796ec857
Paul Goldschmidt <mail@paul-goldschmidt.de>
2025-07-29 14:26:10 -0700

    fix reuse

--- assets/scripts/zlib-unpack/unpack.py
+++ assets/scripts/zlib-unpack/unpack.py
@@ -1,3 +1,11 @@
+#
+# This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+#
+#SPDX-FileCopyrightText: 2023 Stanford University
+#
+# SPDX-License-Identifier: MIT
+#
+
 #!/usr/bin/env python3
 """
 Minimal script to unpack zlib compressed files.
