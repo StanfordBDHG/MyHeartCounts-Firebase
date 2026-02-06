af06b9585844ad68b48fa6ecb5db5c8b8495487b
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-02-07 00:07:45 +0300

    fix for smoking status codes

--- functions/src/services/questionnaireResponse/nicotineScoringService.ts
+++ functions/src/services/questionnaireResponse/nicotineScoringService.ts
@@ -47,15 +47,17 @@ export class DefaultNicotineScoreCalculator implements NicotineScoreCalculator {
   private smokingStatusToScore(smokingStatus: string): number | null {
     // Lookup table based on iOS NicotineExposureCategoryValues enum: https://github.com/StanfordBDHG/MyHeartCounts-iOS/blob/main/MyHeartCounts/Heart%20Health%20Dashboard/CustomHealthSample.swift
     switch (smokingStatus) {
-      case "Never Smoked":
+      case "never-smoked/vaped":
         return 0;
-      case "Quit more than 5 years ago":
+      case "quit->5-years-ago":
         return 1;
-      case "Quit 1 to 5 years ago":
+      case "quit-1-5-years-ago":
         return 2;
-      case "Quit within last year, or am using NDS":
+      case "quit-<1-year-ago":
         return 3;
-      case "Actively smoking":
+      case "light-smoker/vaper-(<10/day)":
+      case "moderate-smoker/vaper-(10-to-19/day)":
+      case "heavy-smoker/vaper-(>20/day)":
         return 4;
       default:
         logger.warn(`Unknown smoking status: ${smokingStatus}`);
@@ -142,11 +144,11 @@ export class NicotineScoringQuestionnaireResponseService extends QuestionnaireRe
       }
 
       const answer = responseItem.answer[0];
-      if (answer.valueCoding?.display) {
-        return answer.valueCoding.display;
+      if (answer.valueCoding?.code) {
+        return answer.valueCoding.code;
       }
 
-      logger.warn(`No valueCoding.display found for linkId '${linkId}'`);
+      logger.warn(`No valueCoding.code found for linkId '${linkId}'`);
       return null;
     } catch (error) {
       logger.warn(
