2a909f2b12347d429b305954c4b97a41577bde1c
Lukas Kollmer <hey@lukaskollmer.de>
2026-02-10 18:55:50 +0100

    fix withdrawal function (#111)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -53,4 +53,5 @@ export * from "./functions/deleteHealthSamples.js";
 export * from "./functions/onArchivedLiveHealthSampleUploaded.js";
 export * from "./functions/markAccountForDeletion.js";
 export * from "./functions/markAccountForStudyReenrollment.js";
+export * from "./functions/markAccountForStudyWithdrawal.js";
 export * from "./functions/processUserDeletions.js";
