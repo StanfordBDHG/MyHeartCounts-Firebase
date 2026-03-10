107f8720c386e450bc81f1e32af461ce31b3c88e
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-03-10 10:43:19 +0100

    fix eslint setup

--- functions/eslint.config.cjs
+++ functions/eslint.config.cjs
@@ -3,10 +3,10 @@
 // SPDX-FileCopyrightText: 2025, 2026 Stanford University and the project authors (see CONTRIBUTORS.md)
 // SPDX-License-Identifier: MIT
 
-const { getEslintConfig } = require('@stanfordspezi/spezi-web-configurations')
+const { getEslintNodeConfig } = require('@stanfordspezi/spezi-web-configurations')
 
 module.exports = [
-  ...getEslintConfig({ tsconfigRootDir: __dirname }),
+  ...getEslintNodeConfig({ tsconfigRootDir: __dirname }),
   {
     ignores: ['lib/**/*', 'node_modules/**/*'],
   },
